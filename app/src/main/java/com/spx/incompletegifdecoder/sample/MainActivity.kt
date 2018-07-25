80caab782761d11f295fd8b542b2de148065ed7e
shaopengxiang <shaopengxiang@163.com>
2018-07-25 11:24:43 +0800

    update

--- app/src/main/java/com/spx/incompletegifdecoder/sample/MainActivity.kt
+++ app/src/main/java/com/spx/incompletegifdecoder/sample/MainActivity.kt
@@ -16,18 +16,4 @@ class MainActivity : AppCompatActivity() {
         // Example of a call to a native method
         Glide.with(this).load(imgUrl).into(gifiv);
     }
-
-//    /**
-//     * A native method that is implemented by the 'native-lib' native library,
-//     * which is packaged with this application.
-//     */
-//    external fun stringFromJNI(): String
-//
-//    companion object {
-//
-//        // Used to load the 'native-lib' library on application startup.
-//        init {
-//            System.loadLibrary("native-lib")
-//        }
-//    }
 }
